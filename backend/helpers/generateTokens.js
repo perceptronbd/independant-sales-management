8975a4ec7e379dd5fb779c9b9c6c63bbdce6a330
asif-bhuti <asif.vudi@northsouth.edu>
2023-06-14 04:02:21 +0600

    update refreshtoken expire time

--- backend/helpers/generateTokens.js
+++ backend/helpers/generateTokens.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 export const generateAccessToken = (user) => {
   return jwt.sign({ id: user._id, role: user.role }, process.env.SECRET_KEY, {
-    expiresIn: "5s",
+    expiresIn: "5m",
   });
 };
 
