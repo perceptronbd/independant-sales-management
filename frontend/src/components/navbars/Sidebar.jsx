4a103816d0ee4d32c9846d119616ac8dc181ed4d
asif-bhuti <asif.vudi@northsouth.edu>
2023-06-15 01:09:33 +0600

    Deny User Access for User/CO User Roles #4: Create and implement link and component view based on role

--- frontend/src/components/navbars/Sidebar.jsx
+++ frontend/src/components/navbars/Sidebar.jsx
@@ -1,99 +1,78 @@
 import { Navbar, Group, Text, ScrollArea, createStyles } from "@mantine/core";
-import {
-  IconBrandEdge,
-  IconUserCircle,
-  IconLayoutDashboard,
-  IconAdjustments,
-  IconNotes,
-  IconSettings,
-  IconFolder,
-  IconUsers,
-  IconFingerprint,
-  IconKey,
-  IconBellCog,
-  IconSwitchHorizontal,
-  IconReportAnalytics,
-  IconAddressBook,
-  IconAffiliate,
-  IconChecklist,
-  IconShoppingBag,
-  IconUserPlus,
-  IconGavel,
-  IconTableAlias,
-} from "@tabler/icons-react";
-import { NavLink } from "react-router-dom";
+import { IconBrandEdge } from "@tabler/icons-react";
 import { IconLogout } from "@tabler/icons-react";
 import { ButtonIcon } from "../buttons/ButtonIcon";
 import { LinksGroup } from "./LinksGroup";
+import { navlink } from "./navlink";
+import { useEffect, useState } from "react";
 
-const navlink = [
-  { label: "Profile", icon: IconUserCircle, links: "/home/profile" },
-  {
-    label: "Dashboard",
-    icon: IconLayoutDashboard,
-    initiallyOpened: false,
-    links: [
-      { label: "CRM", link: "/home/crm", icon: IconTableAlias },
-      {
-        label: "Admin Contact",
-        link: "/home/admin-contact",
-        icon: IconAddressBook,
-      },
-    ],
-  },
-  {
-    label: "Management",
-    icon: IconAdjustments,
-    links: [
-      { label: "Members", link: "/home/members", icon: IconAffiliate },
-      { label: "Check Out", link: "/home/check-out", icon: IconChecklist },
-      {
-        label: "Purchase Order",
-        link: "/home/create-purchase-order",
-        icon: IconShoppingBag,
-      },
-      { label: "Create User", link: "/home/create-user", icon: IconUserPlus },
-    ],
-  },
-  {
-    label: "Reports & Analytics",
-    icon: IconNotes,
-    links: [
-      { label: "Sales Report", link: "/home/sales", icon: IconReportAnalytics },
-      {
-        label: "Legals Agreements",
-        link: "/home/legals-agreements",
-        icon: IconGavel,
-      },
-    ],
-  },
-  {
-    label: "Documents",
-    icon: IconFolder,
-    links: "/home/shared-documents",
-  },
-  {
-    label: "Settings",
-    icon: IconSettings,
-    links: [
-      {
-        link: "/home/user-management",
-        label: "User Management",
-        icon: IconUsers,
-      },
-      {
-        link: "/home/authentications",
-        label: "Users & Authentications",
-        icon: IconKey,
-      },
-      {
-        link: "/home/notifications",
-        label: "Notifications",
-        icon: IconBellCog,
-      },
-    ],
-  },
-];
+//   { label: "Profile", icon: IconUserCircle, links: "/home/profile" },
+//   {
+//     label: "Dashboard",
+//     icon: IconLayoutDashboard,
+//     initiallyOpened: false,
+//     links: [
+//       { label: "CRM", link: "/home/crm", icon: IconTableAlias },
+//       {
+//         label: "Admin Contact",
+//         link: "/home/admin-contact",
+//         icon: IconAddressBook,
+//       },
+//     ],
+//   },
+//   {
+//     label: "Management",
+//     icon: IconAdjustments,
+//     links: [
+//       { label: "Members", link: "/home/members", icon: IconAffiliate },
+//       { label: "Check Out", link: "/home/check-out", icon: IconChecklist },
+//       {
+//         label: "Purchase Order",
+//         link: "/home/create-purchase-order",
+//         icon: IconShoppingBag,
+//       },
+//       { label: "Create User", link: "/home/create-user", icon: IconUserPlus },
+//     ],
+//   },
+//   {
+//     label: "Reports & Analytics",
+//     icon: IconNotes,
+//     links: [
+//       { label: "Sales Report", link: "/home/sales", icon: IconReportAnalytics },
+//       {
+//         label: "Legals Agreements",
+//         link: "/home/legals-agreements",
+//         icon: IconGavel,
+//       },
+//     ],
+//   },
+//   {
+//     label: "Documents",
+//     icon: IconFolder,
+//     links: "/home/shared-documents",
+//   },
+//   {
+//     label: "Settings",
+//     icon: IconSettings,
+//     links: [
+//       {
+//         link: "/home/user-management",
+//         label: "User Management",
+//         icon: IconUsers,
+//       },
+//       {
+//         link: "/home/authentications",
+//         label: "Users & Authentications",
+//         icon: IconKey,
+//       },
+//       {
+//         link: "/home/notifications",
+//         label: "Notifications",
+//         icon: IconBellCog,
+//       },
+//     ],
+//   },
+// ];
 
 const useStyles = createStyles((theme) => ({
   navbar: {
@@ -107,9 +86,6 @@ const useStyles = createStyles((theme) => ({
     paddingTop: 0,
     margin: `0 auto 0 auto`,
     color: theme.colorScheme === "dark" ? theme.white : theme.black,
-    // borderBottom: `${rem(1)} solid ${
-    //   theme.colorScheme === "dark" ? theme.colors.dark[4] : theme.colors.gray[3]
-    // }`,
   },
 
   links: {
@@ -129,9 +105,25 @@ const useStyles = createStyles((theme) => ({
 
 export function Sidebar({ handleLogout }) {
   const { classes } = useStyles();
-  const links = navlink.map((item) => (
-    <LinksGroup {...item} key={item.label} />
-  ));
+
+  const [role, setRole] = useState("");
+
+  const showLinks = (roleLinks) => {
+    return roleLinks.includes(role);
+  };
+
+  const links = navlink.map((item) => {
+    if (showLinks(item.roles)) {
+      return <LinksGroup {...item} key={item.label} />;
+    } else return null;
+  });
+
+  useEffect(() => {
+    setRole(() => {
+      const userData = JSON.parse(localStorage.getItem("user"));
+      return userData ? userData.role : null;
+    });
+  }, []);
 
   return (
     <>
---
+++ frontend/src/components/navbars/navlink.js
@@ -0,0 +1,120 @@
+import {
+  IconUserCircle,
+  IconLayoutDashboard,
+  IconAdjustments,
+  IconNotes,
+  IconSettings,
+  IconFolder,
+  IconUsers,
+  IconKey,
+  IconBellCog,
+  IconReportAnalytics,
+  IconAddressBook,
+  IconAffiliate,
+  IconChecklist,
+  IconShoppingBag,
+  IconUserPlus,
+  IconGavel,
+  IconTableAlias,
+} from "@tabler/icons-react";
+
+export const navlink = [
+  {
+    label: "Profile",
+    icon: IconUserCircle,
+    links: "/home/profile",
+    roles: [
+      "manager",
+      "generator-leader",
+      "generator",
+      "prescriptor",
+      "agent",
+      "user",
+    ],
+  },
+  {
+    label: "Dashboard",
+    icon: IconLayoutDashboard,
+    initiallyOpened: false,
+    links: [
+      { label: "CRM", link: "/home/crm", icon: IconTableAlias },
+      {
+        label: "Admin Contact",
+        link: "/home/admin-contact",
+        icon: IconAddressBook,
+      },
+    ],
+    roles: [
+      "manager",
+      "generator-leader",
+      "generator",
+      "prescriptor",
+      "agent",
+      "user",
+    ],
+  },
+  {
+    label: "Management",
+    icon: IconAdjustments,
+    links: [
+      { label: "Members", link: "/home/members", icon: IconAffiliate },
+      { label: "Check Out", link: "/home/check-out", icon: IconChecklist },
+      {
+        label: "Purchase Order",
+        link: "/home/create-purchase-order",
+        icon: IconShoppingBag,
+      },
+      { label: "Create User", link: "/home/create-user", icon: IconUserPlus },
+    ],
+    roles: ["manager", "generator-leader", "generator", "prescriptor", "agent"],
+  },
+  {
+    label: "Reports & Analytics",
+    icon: IconNotes,
+    links: [
+      { label: "Sales Report", link: "/home/sales", icon: IconReportAnalytics },
+      {
+        label: "Legals Agreements",
+        link: "/home/legals-agreements",
+        icon: IconGavel,
+      },
+    ],
+    roles: ["manager", "generator-leader", "generator", "prescriptor"],
+  },
+  {
+    label: "Documents",
+    icon: IconFolder,
+    links: "/home/shared-documents",
+
+    roles: [
+      "manager",
+      "generator-leader",
+      "generator",
+      "prescriptor",
+      "agent",
+      "user",
+    ],
+  },
+  {
+    label: "Settings",
+    icon: IconSettings,
+    links: [
+      {
+        link: "/home/user-management",
+        label: "User Management",
+        icon: IconUsers,
+      },
+      {
+        link: "/home/authentications",
+        label: "Users & Authentications",
+        icon: IconKey,
+      },
+      {
+        link: "/home/notifications",
+        label: "Notifications",
+        icon: IconBellCog,
+      },
+    ],
+    roles: ["manager"],
+  },
+];
