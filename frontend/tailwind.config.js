dde3d08162a4ad7b47c23e93b47e213d81e4730c
Asif Vudi <asifaslam.aa81@gmail.com>
2023-07-13 23:05:23 +0600

    Add purge to tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  purge: {
+    content: ["./pages/**/*.tsx", "./components/**/*.tsx"],
+    // These options are passed through directly to PurgeCSS
+  },
   theme: {
     extend: {
       colors: {
